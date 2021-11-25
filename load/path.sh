0ce2265410b7007c8bc98e632bcffa4690001dfc
Andrea Cipriani <andreacipriani89@gmail.com>
2021-11-25 16:56:58 -0500

    Google cloud paths

--- load/path.sh
+++ load/path.sh
@@ -1,3 +1,9 @@
 # Add each topic folder to fpath so that they can add functions and completion scripts
 for topic_folder ($DOTFILES/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
 export PATH="$PATH:/$HOME/Library/Android/sdk/platform-tools/"
+export PATH="$PATH:/usr/bin:/usr/local/bin/"
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '~/google-cloud-sdk/path.zsh.inc' ]; then . '~/google-cloud-sdk/path.zsh.inc'; fi
+# The next line enables shell command completion for gcloud.
+if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then . '~/google-cloud-sdk/completion.zsh.inc'; fi
