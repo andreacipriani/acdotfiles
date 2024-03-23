e6a208058031e81a146a0f66210bfc447c08822f
Andrea Cipriani <andreacipriani89@gmail.com>
2024-03-23 17:23:12 -0400

    Update gcloud setup

--- load/path.sh
+++ load/path.sh
@@ -4,6 +4,5 @@ export PATH="$PATH:/$HOME/Library/Android/sdk/platform-tools/"
 export PATH="$PATH:/usr/bin:/usr/local/bin/"
 
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '~/google-cloud-sdk/path.zsh.inc' ]; then . '~/google-cloud-sdk/path.zsh.inc'; fi
-# The next line enables shell command completion for gcloud.
-if [ -f '~/google-cloud-sdk/completion.zsh.inc' ]; then . '~/google-cloud-sdk/completion.zsh.inc'; fi
+source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
+source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
